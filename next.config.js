8481cbc5797c319dc72e32c32528c57842a8bb7c
tsots <mosalatsotetsi@gmail.com>
2022-11-28 06:22:58 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: !debug ? "/taim" : "",
   experimental: {
-    // appDir: true,
+    appDir: true,
     images: {
       allowFutureImage: true,
     },
